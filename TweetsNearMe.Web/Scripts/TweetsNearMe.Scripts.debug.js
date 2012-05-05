918614c2c2e3e0cbd3ce2869a081a24eba1d800b
Brian Hamachek <brian@wnmlive.com>
2012-05-05 02:11:04 -0700

    Initial Check-in

---
+++ TweetsNearMe.Web/Scripts/TweetsNearMe.Scripts.debug.js
@@ -0,0 +1,18 @@
+//! TweetsNearMe.Scripts.debug.js
+//
+
+(function($) {
+
+Type.registerNamespace('TweetsNearMe.Scripts');
+
+////////////////////////////////////////////////////////////////////////////////
+// TweetsNearMe.Scripts.Class1
+
+TweetsNearMe.Scripts.Class1 = function TweetsNearMe_Scripts_Class1() {
+}
+
+
+TweetsNearMe.Scripts.Class1.registerClass('TweetsNearMe.Scripts.Class1');
+})(jQuery);
+
+//! This script was generated using Script# v0.7.4.0
---
+++ TweetsNearMe.Web/Scripts/TweetsNearMe.Scripts.js
@@ -0,0 +1,4 @@
+// TweetsNearMe.Scripts.js
+(function($){
+Type.registerNamespace('TweetsNearMe.Scripts');TweetsNearMe.Scripts.Class1=function(){}
+TweetsNearMe.Scripts.Class1.registerClass('TweetsNearMe.Scripts.Class1');})(jQuery);// This script was generated using Script# v0.7.4.0
